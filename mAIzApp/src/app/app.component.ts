eae23700211c44a31b35fd2b2214b659a7c53c95
EMMYMP20e <emmy.mp.98e@gmail.com>
2020-10-31 19:43:30 -0600

    Mostrar Perfil

--- mAIzApp/src/app/app.component.ts
+++ mAIzApp/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
     },
     {
       title: 'Perfil',
-      url: '/folder/Archived',
+      url: '/perfil',
       icon: 'person-circle'
     }
   ];
